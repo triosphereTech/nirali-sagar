d51c6208cd20c78219c87befe48a99abf56da21c
krunal <krunalmistry2510@gmail.com>
2026-09-26 14:15:57 +0530

    Favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,28 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Nirali & Sagar - Baby Shower Celebration",
-  description: "Join us for a joyous celebration of Nirali and Sagar's upcoming bundle of joy! We can't wait to share this special moment with you...",
+  description:
+    "Join us for a joyous celebration of Nirali and Sagar's upcoming bundle of joy! We can't wait to share this special moment with you...",
+
+  icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        sizes: "any",
+      },
+      {
+        url: "/favicon-16x16.png",
+        type: "image/png",
+        sizes: "16x16",
+      },
+      {
+        url: "/favicon-32x32.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -22,7 +43,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${montserrat.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
